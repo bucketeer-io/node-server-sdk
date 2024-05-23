b4d9b3b9224154575a4229c30d0aef9ab77ddef2
duyhungtnn <duyhung71089@gmail.com>
2024-05-23 16:21:07 +0700

    chore: add the source ID and sdk version to the register events API (#58)

--- src/objects/request.ts
+++ src/objects/request.ts
@@ -2,13 +2,17 @@ import { Event } from './event';
 import { SourceId } from './sourceId';
 import { User } from './user';
 
-export type RegisterEventsRequest = {
+export type BaseRequest = {
+  sourceId: SourceId;
+  sdkVersion: string;
+};
+
+export type RegisterEventsRequest = BaseRequest & {
   events: Event[];
 };
 
-export type GetEvaluationRequest = {
+export type GetEvaluationRequest = BaseRequest & {
   tag: string;
   user?: User;
   featureId: string;
-  sourceId: SourceId;
 };
