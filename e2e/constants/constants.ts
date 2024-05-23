1b171ed0c1ff755d284fc92a668e81e00074081b
duyhungtnn <duyhung71089@gmail.com>
2024-05-23 12:13:15 +0700

    test: update nodejs flags

--- e2e/constants/constants.ts
+++ e2e/constants/constants.ts
@@ -1,13 +1,13 @@
 export const HOST = process.env.HOST!;
 export const TOKEN = process.env.TOKEN!;
-export const FEATURE_TAG = 'javascript'
-export const USER_ID = 'bucketeer-js-user-id-1'
+export const FEATURE_TAG = 'nodejs'
+export const USER_ID = 'bucketeer-nodejs-server-user-id-1'
 
-export const FEATURE_ID_BOOLEAN = 'feature-js-e2e-boolean'
-export const FEATURE_ID_STRING = 'feature-js-e2e-string'
-export const FEATURE_ID_INT = 'feature-js-e2e-int'
-export const FEATURE_ID_DOUBLE = 'feature-js-e2e-double'
-export const FEATURE_ID_JSON = 'feature-js-e2e-json'
+export const FEATURE_ID_BOOLEAN = 'feature-nodejs-server-e2e-boolean'
+export const FEATURE_ID_STRING = 'feature-nodejs-server-e2e-string'
+export const FEATURE_ID_INT = 'feature-nodejs-server-e2e-int'
+export const FEATURE_ID_FLOAT = 'feature-nodejs-server-e2e-float'
+export const FEATURE_ID_JSON = 'feature-nodejs-server-e2e-json'
 
-export const GOAL_ID = 'goal-js-e2e-1'
+export const GOAL_ID = 'goal-nodejs-server-e2e-1'
 export const GOAL_VALUE = 1.0
--- e2e/evaluations.ts
+++ e2e/evaluations.ts
@@ -1,6 +1,6 @@
 import anyTest, { TestFn } from 'ava';
 import { Bucketeer, DefaultLogger, User, initialize } from '../lib';
-import { HOST, TOKEN, FEATURE_TAG, USER_ID, FEATURE_ID_BOOLEAN, FEATURE_ID_STRING, FEATURE_ID_INT, FEATURE_ID_DOUBLE, FEATURE_ID_JSON } from './constants/constants';
+import { HOST, TOKEN, FEATURE_TAG, USER_ID, FEATURE_ID_BOOLEAN, FEATURE_ID_STRING, FEATURE_ID_INT, FEATURE_ID_DOUBLE, FEATURE_ID_JSON, FEATURE_ID_FLOAT } from './constants/constants';
 
 
 const test = anyTest as TestFn<{ bktClient: Bucketeer; user: User }>;
@@ -19,25 +19,23 @@ test.beforeEach((t) => {
 
 test('boolVariation', async (t) => {
   const { bktClient, user } = t.context;
-  t.is(await bktClient.getBoolVariation(user, FEATURE_ID_BOOLEAN, false), true);
+  t.is(await bktClient.getBoolVariation(user, FEATURE_ID_BOOLEAN, true), false);
 });
 
 test('stringVariation', async (t) => {
   const { bktClient, user } = t.context;
-  t.is(await bktClient.getStringVariation(user, FEATURE_ID_STRING, ''), 'value-1');
+  t.is(await bktClient.getStringVariation(user, FEATURE_ID_STRING, ''), 'value-2');
 });
 
 test('numberVariation', async (t) => {
   const { bktClient, user } = t.context;
-  t.is(await bktClient.getNumberVariation(user, FEATURE_ID_INT, 0), 10);
-  t.is(await bktClient.getNumberVariation(user, FEATURE_ID_DOUBLE, 0.0), 2.1);
+  t.is(await bktClient.getNumberVariation(user, FEATURE_ID_INT, 0), 20);
+  t.is(await bktClient.getNumberVariation(user, FEATURE_ID_FLOAT, 0.0), 3.1);
 });
 
 test('jsonVariation', async (t) => {
   const { bktClient, user } = t.context;
-  t.deepEqual(await bktClient.getJsonVariation(user, FEATURE_ID_JSON, {}), {
-    key: 'value-1',
-  });
+  t.deepEqual(await bktClient.getJsonVariation(user, FEATURE_ID_JSON, {}),  {"str":"str2", "int": "int2"});
 });
 
 test.afterEach(async (t) => {
