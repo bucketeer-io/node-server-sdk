7361f4dd7f64c41141ee54088d273daf7b2d6dd8
duyhungtnn <duyhung71089@gmail.com>
2024-05-23 10:55:49 +0700

    test: add e2e test for checking invalid api_key

---
+++ e2e/client.ts
@@ -0,0 +1,15 @@
+import test from 'ava'
+import { initialize, DefaultLogger } from '../lib';
+import { HOST, TOKEN, FEATURE_TAG, USER_ID, FEATURE_ID_BOOLEAN } from './constants/constants';
+
+test('Using a random string in the api key setting should not throw exception', async (t) => {
+    const bktClient = initialize({
+        host: HOST,
+        token: "TOKEN_RANDOM",
+        tag: FEATURE_TAG,
+        logger: new DefaultLogger("error")
+      });
+    const user = { id: USER_ID, data: {} }
+    const result = await t.notThrowsAsync(bktClient.getBoolVariation(user, FEATURE_ID_BOOLEAN, true));
+    t.true(result);
+  });
