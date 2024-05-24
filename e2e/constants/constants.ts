fbb94d281364d52740ec2323055227211c272d29
duyhungtnn <duyhung71089@gmail.com>
2024-05-24 08:57:52 +0700

    test: add e2e tests (#57)

---
+++ e2e/constants/constants.ts
@@ -0,0 +1,13 @@
+export const HOST = process.env.HOST!;
+export const TOKEN = process.env.TOKEN!;
+export const FEATURE_TAG = 'nodejs'
+export const TARGETED_USER_ID = 'bucketeer-nodejs-server-user-id-1'
+
+export const FEATURE_ID_BOOLEAN = 'feature-nodejs-server-e2e-boolean'
+export const FEATURE_ID_STRING = 'feature-nodejs-server-e2e-string'
+export const FEATURE_ID_INT = 'feature-nodejs-server-e2e-int'
+export const FEATURE_ID_FLOAT = 'feature-nodejs-server-e2e-float'
+export const FEATURE_ID_JSON = 'feature-nodejs-server-e2e-json'
+
+export const GOAL_ID = 'goal-nodejs-server-e2e-1'
+export const GOAL_VALUE = 1.0
