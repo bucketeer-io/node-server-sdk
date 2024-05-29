c872ad558de59ff2197114ee2caeb601e1b906be
duyhungtnn <duyhung71089@gmail.com>
2024-05-29 08:55:43 +0700

    test: update metrics tests

--- e2e/client.ts
+++ e2e/client.ts
@@ -23,19 +23,34 @@ test('Using a random string in the api key setting should not throw exception', 
   // Can not load the evaluation (the correct value should be `false`, but we will received the default value `true`)
   const result = await t.notThrowsAsync(bktClient.getBoolVariation(user, FEATURE_ID_BOOLEAN, true));
   t.true(result);
+
+  const bktClientImpl = bktClient as BKTClientImpl
+  const events = bktClientImpl.eventStore.getAll()
+  t.true(events.some((e) => {
+
+    if (isMetricsEvent(e.event)) {
+      const metrics = e.event as MetricsEvent
+      return metrics.event?.['@type'] === FORBIDDEN_ERROR_METRICS_EVENT_NAME && metrics.event?.apiId === ApiId.GET_EVALUATION
+    }
+    return false;
+  }));
 });
 
 test('altering featureTag should not affect api request', async (t) => {
   const config = {
     host: HOST,
-    token: "TOKEN_RANDOM",
+    token: TOKEN,
     tag: FEATURE_TAG,
     logger: new DefaultLogger("error")
   }
   const bktClient = initialize(config);
   const user = { id: TARGETED_USER_ID, data: {} }
-  const result = await t.notThrowsAsync(bktClient.getBoolVariation(user, FEATURE_ID_BOOLEAN, true));
+  const result = await t.notThrowsAsync(bktClient.getBoolVariation(user, FEATURE_ID_BOOLEAN, false));
   t.true(result);
+  config.tag = "RANDOME"
+
+  const resultAfterAlterAPIKey = await t.notThrowsAsync(bktClient.getBoolVariation(user, FEATURE_ID_BOOLEAN, false));
+  t.true(resultAfterAlterAPIKey);
 });
 
 test('Altering the api key should not affect api request', async (t) => {
@@ -71,7 +86,6 @@ test('Using a random string in the featureTag setting should affect api request'
   const bktClientImpl = bktClient as BKTClientImpl
   const events = bktClientImpl.eventStore.getAll()
   t.true(events.some((e) => {
-
     if (isMetricsEvent(e.event)) {
       const metrics = e.event as MetricsEvent
       return metrics.event?.['@type'] === NOT_FOUND_ERROR_METRICS_EVENT_NAME && metrics.event?.apiId === ApiId.GET_EVALUATION
