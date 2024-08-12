8dc1c1729c2cdeeac069d0a1c18c58e91f89e221
duyhungtnn <duyhung71089@gmail.com>
2024-08-12 16:15:47 +0700

    tests: convert string to num

--- src/__tests__/converter_string_to_num.ts
+++ src/__tests__/converter_string_to_num.ts
@@ -1,9 +1,9 @@
 import test from 'ava';
-import { stringToBoolConverter } from '../converter';
+import { stringToBoolConverter, stringToNumberConverter } from '../converter';
 
 type StringToNumConvertTestCase = {
   input: string;
-  expected: boolean | null;
+  expected: number | null;
 };
 
 // List of test cases
@@ -12,18 +12,22 @@ const stringConvertTestCases: StringToNumConvertTestCase[] = [
   { input: 'default false', expected: null },
   { input: ' ', expected: null },
   { input: '', expected: null },
-  { input: '1', expected: null },
-  { input: '2', expected: null },
+  { input: '1', expected: 1 },
+  { input: '2', expected: 2 },
+  { input: '0.1', expected: 0.1 },
+  { input: '12.1', expected: 12.1 },
+  { input: '12.0', expected: 12 },
+  { input: '1', expected: 1 },
   { input: '[]', expected: null },
   { input: '{"key1": "value1"}', expected: null },
-  { input: 'true', expected: true },
-  { input: 'false', expected: false },
+  { input: 'true', expected: null },
+  { input: 'false', expected: null },
 ];
 
 stringConvertTestCases.forEach(({ input, expected }, index) => {
   test(`stringToBoolConverter test case ${index + 1} input: ${input} - expected: ${expected}`, (t) => {
     try {
-      const output = stringToBoolConverter(input);
+      const output = stringToNumberConverter(input);
       t.is(output, expected);
     } catch (err) {
       t.is(expected, null);
