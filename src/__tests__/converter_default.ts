2e8cca3cbb3e4a741e404ef81d25c950cdcf917d
duyhungtnn <duyhung71089@gmail.com>
2024-08-12 15:58:55 +0700

    test: add type converter tests

---
+++ src/__tests__/converter_default.ts
@@ -0,0 +1,28 @@
+import test from 'ava';
+import { defaultStringToTypeConverter } from '../converter';
+
+type StringConvertTestCase = {
+  input: string;
+  expected: string;
+};
+
+// List of test cases
+const stringConvertTestCases: StringConvertTestCase[] = [
+  { input: 'default true', expected: 'default true' },
+  { input: 'default false', expected: 'default false' },
+  { input: ' ', expected: ' ' },
+  { input: '', expected: '' },
+  { input: '1', expected: '1' },
+  { input: '2', expected: '2' },
+  { input: '2.0', expected: '2.0' },
+  { input: '12.1', expected: '12.1' },
+  { input: '[]', expected: '[]' },
+  { input: '{"key1": "value1"}', expected: '{"key1": "value1"}' },
+];
+
+stringConvertTestCases.forEach(({ input, expected }, index) => {
+  test(`defaultStringToTypeConverter test case ${index + 1}`, (t) => {
+    const output = defaultStringToTypeConverter(input);
+    t.is(output, expected);
+  });
+});
---
+++ src/__tests__/converter_string_to_num.ts
@@ -0,0 +1,32 @@
+import test from 'ava';
+import { stringToBoolConverter } from '../converter';
+
+type StringToNumConvertTestCase = {
+  input: string;
+  expected: boolean | null;
+};
+
+// List of test cases
+const stringConvertTestCases: StringToNumConvertTestCase[] = [
+  { input: 'default true', expected: null },
+  { input: 'default false', expected: null },
+  { input: ' ', expected: null },
+  { input: '', expected: null },
+  { input: '1', expected: null },
+  { input: '2', expected: null },
+  { input: '[]', expected: null },
+  { input: '{"key1": "value1"}', expected: null },
+  { input: 'true', expected: true },
+  { input: 'false', expected: false },
+];
+
+stringConvertTestCases.forEach(({ input, expected }, index) => {
+  test(`stringToBoolConverter test case ${index + 1} input: ${input} - expected: ${expected}`, (t) => {
+    try {
+      const output = stringToBoolConverter(input);
+      t.is(output, expected);
+    } catch (err) {
+      t.is(expected, null);
+    }
+  });
+});
