4dc80e6be38ca5b545dce992ce06a0073be07115
duyhungtnn <duyhung71089@gmail.com>
2026-05-13 17:01:53 +0700

    test(e2e): load secret from env (#196)

--- ava-e2e.config.mjs
+++ ava-e2e.config.mjs
@@ -1,3 +1,5 @@
+import 'dotenv/config'; // Loads .env file into process.env
+
 export default {
   babel: {
     testOptions: {
@@ -9,10 +11,4 @@ export default {
     '__e2e/__test__/*.js',
     '__e2e/__test__/local_evaluation/*.js'
   ],
-  environmentVariables: {
-    API_ENDPOINT: '<API_ENDPOINT>', // replace this. e.g. api.example.com (without scheme)
-    SCHEME: '<SCHEME>', // replace this. e.g. https or http (defaults to https)
-    CLIENT_API_KEY: '<CLIENT_API_KEY>', // replace this. Client SDK role API key
-    SERVER_API_KEY: '<SERVER_API_KEY>', // replace this. Server SDK role API key for testing with local evaluate
-  },
 };
